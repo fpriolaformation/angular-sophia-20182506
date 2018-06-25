536a315e5f2832907ac48b4c072ed33b66211fa4
fpriolaformation <franckpriola@gmail.com>
2018-06-25 14:32:23 +0200

    architecturemodulaire

--- app-formation/src/app/app.module.ts
+++ app-formation/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CoreModule,
+    SharedModule,
+    BrowserModule,
+    HomeModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
